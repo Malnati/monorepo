90608e86eec41764d9094042a657c45e179a9a5a
Malnati <ricardomalnati@gmail.com>
2025-11-21 19:52:31 +0100

    swagger

--- app/api/src/main.ts
+++ app/api/src/main.ts
@@ -1,6 +1,7 @@
 // app/api/src/main.ts
 import { NestFactory } from "@nestjs/core";
 import { ValidationPipe } from "@nestjs/common";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
 import { LoggingInterceptor } from "./common/interceptors/logging.interceptor";
 import express from "express";
@@ -42,8 +43,36 @@ async function bootstrap() {
   // Prefixo global de API
   app.setGlobalPrefix("api");
 
+  // Configuração do Swagger
+  const config = new DocumentBuilder()
+    .setTitle("API - Marketplace de Resíduos")
+    .setDescription("API NestJS para protótipo MVP de marketplace de produtos")
+    .setVersion("0.1.0")
+    .addBearerAuth(
+      {
+        type: "http",
+        scheme: "bearer",
+        bearerFormat: "JWT",
+        name: "JWT",
+        description: "Enter JWT token",
+        in: "header",
+      },
+      "JWT-auth",
+    )
+    .addServer(`http://localhost:${PORT}`, "Desenvolvimento Local")
+    .addServer("https://monorepo.cranio.dev", "Produção")
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api/docs", app, document, {
+    customSiteTitle: "API Documentation",
+    customfavIcon: "/favicon.ico",
+    customCss: ".swagger-ui .topbar { display: none }",
+  });
+
   await app.listen(PORT, HOST);
   console.log(`🚀 API rodando em http://${HOST}:${PORT}/api`);
+  console.log(`📚 Swagger disponível em http://${HOST}:${PORT}/api/docs`);
 }
 
 bootstrap();
